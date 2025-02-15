e9f1cb5e04527b060e22c9bb456104cd853ed0dc
Arpit Kumar <arpitk2806@gmail.com>
2025-02-15 10:08:29 +0100

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
               <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
                 Welcome to NoteBook
               </p>
-              <Link to="/Login"> Takle me Home</Link>
+              <Link class="mt-2 rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" to="/Login"> Sign In</Link>
             </div>
           </div>
         </div>
